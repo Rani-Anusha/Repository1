9bcb85b58349808fe4b0116cb6f9d8b8c1471fe6
mediboyana anusha <mediboyana.anusha@globallogic.com>
2023-12-14 01:36:29 +0000

    3th commit

--- models/stage/stg_customers.sql
+++ models/stage/stg_customers.sql
@@ -8,11 +8,13 @@
 
 select
         c_custkey as customer_id,
-        c_nationkey as nation_id,
+        n.n_name as nation,
         c_name as name,
         c_address as address,
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
         c_comment as comment
-from {{source('src','customers') }}
+from {{source('src','customers') }} as c
+join {{source('src','nations') }} as n
+on c.c_nationkey = n.n_nationkey
